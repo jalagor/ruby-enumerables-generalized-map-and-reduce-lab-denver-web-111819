469cf139fe23937dab04fc922d7f28b7199794db
John Rogala <johnrossrogala@gmail.com>
2019-11-15 12:46:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,7 @@
 def map(source_array) 
   counter = 0 
   answers = []
-  while count < source_array[count] 
+  while count < source_array.count 
   answers<<(yield(source_array[counter])) 
   counter += 1 
 end
